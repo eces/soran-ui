44cc5b75e4cafd2ac324fefa99570b63659675ee
Jinhyuk Lee <eces@mstock.org>
2013-05-07 22:10:24 +0900

    Add index page

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,7 @@ import play.api.mvc._
 object Application extends Controller {
   
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index("소란에서 시작되는 음악 생활 - SORAN"))
   }
   
 }
