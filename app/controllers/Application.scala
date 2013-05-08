1bf737cc4f2f71bc0b8b6ab062382521f352c7e1
Jinhyuk Lee <eces@mstock.org>
2013-05-08 23:25:25 +0900

    Add playlist page

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -8,5 +8,9 @@ object Application extends Controller {
   def index = Action {
     Ok(views.html.index("소란에서 시작되는 음악 생활 - SORAN"))
   }
+
+  def playlist = Action {
+    Ok(views.html.playlist("강효준의 플레이리스트 - 소란 SORAN"))
+  }
   
 }
